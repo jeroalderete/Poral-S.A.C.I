012e43e2a450727003b8b929f30b4f00f217379f
Jeronimo Alderete <jero.alderete@hotmail.com>
2023-04-18 18:05:27 -0300

    migration14

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet as Page } from "react-router-dom";
-import Navbar from "../Navbar/Index";
+import Navbar2 from "../Navbar2/Navbar2.jsx";
 import Footer from "../Footer";
 
 const Layout = () => {
   return (
     <main>
-      <Navbar />
+      <Navbar2 />
       <Page />
       <Footer />
     </main>
---
+++ src/components/Navbar2/Navbar2.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const navlinks = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Obras",
+    link: "/obras",
+  },
+  {
+    title: "Home",
+    link: "/",
+  },
+];
+
+const Navbar2 = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => {
+    setOpen((prev) => !prev);
+    console.log(open);
+  };
+
+  return (
+    <div className="bg-black">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <NavLink to="/">
+              <img
+                className="img-logo"
+                src="../../../public/assets/logo1-2.png"
+              />
+            </NavLink>
+          </div>
+          {/*        navlinks */}
+          <div className="hidden md:block  flex-links">
+            <div className="flex space-x-4 w-full justify-start">
+              <NavLink
+                to="/"
+                className="nav-text text-white hover:text-gray-400 duration-500"
+              >
+                INICIO
+              </NavLink>
+
+              <NavLink
+                to="/about"
+                className="nav-text text-white hover:text-gray-400 duration-500"
+              >
+                NOSOTROS
+              </NavLink>
+
+              <NavLink
+                to="/obras"
+                className="nav-text text-white hover:text-gray-400 duration-500"
+              >
+                OBRAS
+              </NavLink>
+
+              <NavLink
+                to="/contact"
+                className="nav-text text-white hover:text-gray-400 duration-500"
+              >
+                CONTACTO
+              </NavLink>
+            </div>
+          </div>
+          <div className="flex items-center justify-between h-16">
+            {/*    hamburguer button  */}
+            <div>
+              <button
+                type="button"
+                onClick={handleMenu}
+                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-70 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              >
+                <span className="sr-only">Open Main Menu</span>
+                {open == true ? <FaTimes /> : <FaBars />}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/*   mobile-menu */}
+      {open ? (
+        <div className="md:hidden">
+          <div className="ox-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <div>
+              <NavLink
+                to="/"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                INICIO
+              </NavLink>
+
+              <NavLink
+                to="/about"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base  font-medium"
+              >
+                NOSOTROS
+              </NavLink>
+
+              <NavLink
+                to="/obras"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base  font-medium"
+              >
+                OBRAS
+              </NavLink>
+
+              <NavLink
+                to="/contact"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                CONTACTO
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Navbar2;
